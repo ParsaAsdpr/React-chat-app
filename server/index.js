0114e2f673a1219a065c78a5e441a8c1114a4f8b
Parsa <parsaasadpour999@gmail.com>
2022-10-04 23:07:16 +0330

    fixed server bugs

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
-const app = require('express')();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const app = require('express')()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
 
 io.on('connection', socket => {
-    socket.on('message', ({name, message}) => {
-        io.emit('message', {name, message})
-    })
+  socket.on('message', ({ name, message }) => {
+    io.emit('message', { name, message })
+  })
 })
 
-http.listen(3001, () => {
-    console.log('listening on port 3001')
+http.listen(4000, function() {
+  console.log('listening on port 4000')
 })
